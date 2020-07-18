1d13b64b61b38beafb0785b5959c85d52ce81d0f
Alberto <alberto.acerbis@4solid.it>
2020-07-18 15:52:53 +0200

    MufloneBank
    - Add a fake payment system
    - Change icon

---
+++ src/CqrsMovie.MufloneBank/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CqrsMovie.MufloneBank.Controllers
+{
+  public abstract class BaseController: Controller
+  {
+    protected ILogger Logger { get; }
+
+    protected BaseController(ILoggerFactory loggerFactory)
+    {
+      this.Logger = loggerFactory.CreateLogger(this.GetType());
+    }
+  }
+}
