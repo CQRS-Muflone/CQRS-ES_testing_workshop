772b363976e1672f4f3a3ba51491d65304f75122
Alberto <alberto.acerbis@4solid.it>
2020-07-21 16:10:30 +0200

    NuGet
    - Update all Muflone NuGet packages

--- src/CqrsMovie.Website/Startup.cs
+++ src/CqrsMovie.Website/Startup.cs
@@ -1,8 +1,6 @@
 ﻿using CqrsMovie.Website.Infrastructure.MassTransit.Events;
 using CqrsMovie.Website.Infrastructure.MongoDb;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
