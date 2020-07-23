a78342408d9e549b68e05377a6d2c5b8612410aa
Alberto <alberto.acerbis@4solid.it>
2020-07-23 09:08:48 +0200

    Test
    - Update NETCORE for test project

--- src/CqrsMovie.Seats.Domain.Tests/Entities/CreateDailyProgramming_CreateAggregate.cs
+++ src/CqrsMovie.Seats.Domain.Tests/Entities/CreateDailyProgramming_CreateAggregate.cs
@@ -12,39 +12,39 @@ using Muflone.SpecificationTests;
 
 namespace CqrsMovie.Seats.Domain.Tests.Entities
 {
-  public class CreateDailyProgramming_CreateAggregate : CommandSpecification<CreateDailyProgramming>
-  {
-    private readonly DailyProgrammingId aggregateId = new DailyProgrammingId(Guid.NewGuid());
-    private readonly MovieId movieId = new MovieId(Guid.NewGuid());
-    private readonly ScreenId screenId = new ScreenId(Guid.NewGuid());
-    private readonly DateTime dailyDate = DateTime.Today;
-    private readonly string movieTitle = "rambo";
-    private readonly string screenName = "screen 99";
-    private readonly IEnumerable<Seat> seats;
-
-    public CreateDailyProgramming_CreateAggregate()
+    public class CreateDailyProgramming_CreateAggregate : CommandSpecification<CreateDailyProgramming>
     {
-      seats = new List<Seat> { new Seat { Number = 1, Row = "A" } };
-    }
+        private readonly DailyProgrammingId aggregateId = new DailyProgrammingId(Guid.NewGuid());
+        private readonly MovieId movieId = new MovieId(Guid.NewGuid());
+        private readonly ScreenId screenId = new ScreenId(Guid.NewGuid());
+        private readonly DateTime dailyDate = DateTime.Today;
+        private readonly string movieTitle = "rambo";
+        private readonly string screenName = "screen 99";
+        private readonly IEnumerable<Seat> seats;
 
-    protected override IEnumerable<DomainEvent> Given()
-    {
-      return new List<DomainEvent>();
-    }
+        public CreateDailyProgramming_CreateAggregate()
+        {
+            seats = new List<Seat> { new Seat { Number = 1, Row = "A" } };
+        }
 
-    protected override CreateDailyProgramming When()
-    {
-      return new CreateDailyProgramming(aggregateId, movieId, screenId, dailyDate, seats, movieTitle, screenName);
-    }
+        protected override IEnumerable<DomainEvent> Given()
+        {
+            return new List<DomainEvent>();
+        }
 
-    protected override ICommandHandler<CreateDailyProgramming> OnHandler()
-    {
-      return new CreateDailyProgrammingCommandHandler(Repository, new NullLoggerFactory());
-    }
+        protected override CreateDailyProgramming When()
+        {
+            return new CreateDailyProgramming(aggregateId, movieId, screenId, dailyDate, seats, movieTitle, screenName);
+        }
 
-    protected override IEnumerable<DomainEvent> Expect()
-    {
-      yield return new DailyProgrammingCreated(aggregateId, movieId, screenId, dailyDate, seats, movieTitle, screenName);
+        protected override ICommandHandler<CreateDailyProgramming> OnHandler()
+        {
+            return new CreateDailyProgrammingCommandHandler(Repository, new NullLoggerFactory());
+        }
+
+        protected override IEnumerable<DomainEvent> Expect()
+        {
+            yield return new DailyProgrammingCreated(aggregateId, movieId, screenId, dailyDate, seats, movieTitle, screenName);
+        }
     }
-  }
 }
